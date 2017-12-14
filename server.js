fa2d217b0fc660a6004a13797d2528b39c8201df
Kacper Raczy <gfw.kra@gmail.com>
2017-12-14 22:37:33 +0100

    Added handlebars rendering support

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-var express = require("express");
+var express = require('express');
 var bodyParser = require("body-parser");
+var hbs = require('express-handlebars');
+var path = require('path');
 var port = 1337;
 
 var app = express();
+
+//view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views', 'layouts')}));
+app.set('views', path.join(__dirname, 'views', 'templates'));
+app.set('view engine', 'hbs');
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//additional setup
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
+app.get('/', function(req, res) {
+  res.render('home', {title: "Hello World"});
 });
 
 app.listen(port, function() {
