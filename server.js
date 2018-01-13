8499bd40ee8453a6f82e2af70f6d43c680cfd0ab
PanHagrid <fklich@gmail.com>
2018-01-13 16:00:04 +0100

    Merge from kraczy

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ var port = 1337;
 var app = express();
 
 //view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views', 'layouts')}));
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views', 'layouts')
+}));
 app.set('views', path.join(__dirname, 'views', 'templates'));
 app.set('view engine', 'hbs');
 
@@ -27,10 +31,17 @@ app.use(require('./auth').authenticate);
 app.use('/login', require('./controllers/routes/login'));
 app.use('/products', require('./controllers/routes/products'));
 app.use('/contact', require('./controllers/routes/contact'));
+app.use('/admin', require('./controllers/routes/admin'));
 
 //home
 app.get('/', function(req, res) {
-  res.render('home', {user: req.user, categories: [{id:4, name:"Compyterrs"}]});
+  res.render('home', {
+    user: req.user,
+    categories: [{
+      id: 4,
+      name: "Compyterrs"
+    }]
+  });
 });
 
 //restricted
